90b6ccc00b963d5743dcca63706ac49d3e5332d4
solowiejmaciej <55091850+solowiejmaciej@users.noreply.github.com>
2021-04-20 20:24:27 +0200

    20.4.2021

--- 2018/czerwiec/4-3.py
+++ 2018/czerwiec/4-3.py
@@ -3,11 +3,19 @@ plik2 = open("dane2.txt")
 
 dane1=[]
 dane2=[]
-dane1_ostatnie=[]
-dane2_ostatnie=[]
-counter=0
 
 for line in plik1.readlines():
     dane1.append(line.split())
 for line in plik2.readlines():
     dane2.append(line.split())
+
+counter = 0
+for x in range(len(dane1)):
+    liczby_dane1 = list(set(dane1[x]))
+    liczby_dane2 = list(set(dane2[x]))
+    liczby_dane1.sort()
+    liczby_dane2.sort()
+    if liczby_dane2 == liczby_dane1:
+        print(liczby_dane1,liczby_dane2,x+1)
+        counter+=1
+print(counter)
